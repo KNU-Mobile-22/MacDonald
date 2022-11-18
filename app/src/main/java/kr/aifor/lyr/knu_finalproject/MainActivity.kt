07fb64528f5d6db281decc1aefe46a75ff294081
dleh1541 <dleh1541@naver.com>
2022-11-18 14:49:51 +0900

    feature/GeneralMenu branch commit test

--- app/src/main/java/kr/aifor/lyr/knu_finalproject/MainActivity.kt
+++ app/src/main/java/kr/aifor/lyr/knu_finalproject/MainActivity.kt
@@ -2,11 +2,13 @@ package kr.aifor.lyr.knu_finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_main)
         setContentView(R.layout.gen_menu_layout)
+        Log.d("DEBUG", "test")
     }
 }
