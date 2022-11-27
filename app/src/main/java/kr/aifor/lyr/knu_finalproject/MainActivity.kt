6b71f6e0420b9b71fccc5101d3dcc3a2d4258775
dleh1541 <dleh1541@naver.com>
2022-11-27 15:43:25 +0900

    easy menu layout 수정

--- app/src/main/java/kr/aifor/lyr/knu_finalproject/MainActivity.kt
+++ app/src/main/java/kr/aifor/lyr/knu_finalproject/MainActivity.kt
@@ -2,11 +2,23 @@ package kr.aifor.lyr.knu_finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import kr.aifor.lyr.knu_finalproject.databinding.EasyMenuLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_main)
-        setContentView(R.layout.easy_menu_layout)
+
+        var binding = EasyMenuLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
